747a4e42ae2ccbd6e8a5edefc1f97a7d7ebddec0
Amit-kumar99 <amit.kumar.16208932@gmail.com>
2024-02-09 21:17:33 +0530

    wrote logout logic & removing the token from cookie successfully on logout

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,7 +1,28 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage() {
+    const router = useRouter();
+
+    const onLogout = async () => {
+        try {
+            await axios.get("/api/users/logout");
+            router.push("/login");
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
     return (
         <div>
             <h1>Profile</h1>
+            <p>Welcome to Profile page</p>
+            <button 
+                className="mt-4 p-2 bg-blue-500 text-white"
+                onClick={onLogout}>
+                    Logout
+            </button>
         </div>
     )
 }
